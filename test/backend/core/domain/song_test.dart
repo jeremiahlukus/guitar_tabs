f3e7ee85877b8350fe183eb2ccb8d2bea1b34879
Jeremiah Parrack <jeremiahlukus1@gmail.com>
2022-07-08 22:21:44 -0400

    Jeremiah-Parrack | need to setup my presommit hook

--- test/backend/core/domain/song_test.dart
+++ test/backend/core/domain/song_test.dart
@@ -8,14 +8,15 @@ void main() {
   group('Song', () {
     test('creates Song object', () {
       const song = Song(
-          id: 1,
-          title: 'title',
-          songNumber: 1,
-          lyrics: 'lyrics',
-          category: 'category',
-          artist: 'artist',
-          chords: 'chords',
-          url: 'url',);
+        id: 1,
+        title: 'title',
+        songNumber: 1,
+        lyrics: 'lyrics',
+        category: 'category',
+        artist: 'artist',
+        chords: 'chords',
+        url: 'url',
+      );
       const expected = 'title';
 
       final actual = song.title;
