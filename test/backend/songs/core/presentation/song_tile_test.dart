e14c72d0b19d989f23821168dcdab5118bdc3789
Jeremiah Parrack <jeremiahlukus1@gmail.com>
2022-07-18 21:00:48 -0400

    Jeremiah-Parrack | add golden test for songtile

--- test/backend/songs/core/presentation/song_tile_test.dart
+++ test/backend/songs/core/presentation/song_tile_test.dart
@@ -4,10 +4,14 @@ import 'package:flutter/material.dart';
 // Package imports:
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:alchemist/alchemist.dart';
 
 // Project imports:
 import 'package:joyful_noise/backend/core/domain/song.dart';
 import 'package:joyful_noise/backend/songs/core/presentation/song_tile.dart';
+
+import '../../../../utils/device.dart';
+import '../../../../utils/golden_test_device_scenario.dart';
 
 class MockSong extends Mock implements Song {}
 
@@ -57,5 +61,55 @@ void main() {
       final listTileFinder = find.byType(ListTile);
       expect(listTileFinder, findsOneWidget);
     });
+  });
+
+  final song = MockSong();
+  when(() => song.artist).thenReturn('artist');
+  when(() => song.title).thenReturn('title');
+  Widget buildWidgetUnderTest() => const MaterialApp(
+        home: Scaffold(
+          body: SongTile(
+            song: Song(
+              id: 1,
+              title: 'title',
+              lyrics: 'lyrics',
+              category: 'category',
+              artist: 'artist',
+              chords: 'chords',
+              url: 'url',
+              songNumber: 1,
+            ),
+          ),
+        ),
+      );
+
+  group('SongTile Golden Test', () {
+    goldenTest(
+      'renders correctly on mobile',
+      fileName: 'SongTile',
+      builder: () => GoldenTestGroup(
+        children: [
+          GoldenTestDeviceScenario(
+            device: Device.smallPhone,
+            name: 'golden test SongTile on small phone',
+            builder: buildWidgetUnderTest,
+          ),
+          GoldenTestDeviceScenario(
+            device: Device.tabletLandscape,
+            name: 'golden test SongTile on tablet landscape',
+            builder: buildWidgetUnderTest,
+          ),
+          GoldenTestDeviceScenario(
+            device: Device.tabletPortrait,
+            name: 'golden test SongTile on tablet Portrait',
+            builder: buildWidgetUnderTest,
+          ),
+          GoldenTestDeviceScenario(
+            name: 'golden test SongTile on iphone11',
+            builder: buildWidgetUnderTest,
+          ),
+        ],
+      ),
+    );
   });
 }
