aaa7b5f26ccb0c30ee5018de4bf761fe8fde733a
Jeremiah Parrack <jeremiahlukus1@gmail.com>
2022-07-20 11:14:49 -0400

    Jeremiah-Parrack | add victor's tests, and signout test

--- test/backend/songs/favorite_songs/presentation/favorite_songs_page_test.dart
+++ test/backend/songs/favorite_songs/presentation/favorite_songs_page_test.dart
@@ -1,11 +1,167 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+
 // Package imports:
+import 'package:dartz/dartz.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+// Project imports:
+import 'package:joyful_noise/auth/notifiers/auth_notifier.dart';
+import 'package:joyful_noise/auth/shared/providers.dart';
+import 'package:joyful_noise/backend/core/domain/backend_failure.dart';
+import 'package:joyful_noise/backend/core/domain/song.dart';
+import 'package:joyful_noise/backend/core/domain/user.dart';
+import 'package:joyful_noise/backend/core/infrastructure/user_repository.dart';
+import 'package:joyful_noise/backend/core/notifiers/user_notifier.dart';
+import 'package:joyful_noise/backend/core/shared/providers.dart';
+import 'package:joyful_noise/backend/songs/core/notifiers/paginated_songs_notifier.dart';
+import 'package:joyful_noise/backend/songs/core/presentation/paginated_songs_list_view.dart';
+import 'package:joyful_noise/backend/songs/favorite_songs/infrastructure/favorite_songs_repository.dart';
+import 'package:joyful_noise/backend/songs/favorite_songs/notifiers/favorite_song_notifier.dart';
+import 'package:joyful_noise/backend/songs/favorite_songs/presentation/favorite_songs_page.dart';
+import 'package:joyful_noise/core/domain/fresh.dart';
+import 'package:joyful_noise/core/shared/providers.dart';
+
+class MockFavoriteSongRepository extends Mock implements FavoriteSongsRepository {}
+
+class MockUserRepository extends Mock implements UserRepository {}
+
+class FakeUserNotifier extends UserNotifier {
+  FakeUserNotifier(UserRepository userRepository) : super(userRepository);
+
+  @override
+  Future<void> getUserPage() async {
+    state = const UserState.loadSuccess(
+      User(name: 'Jon Doe', avatarUrl: 'www.example.com/avatarUrl'),
+    );
+    return;
+  }
+}
+
+class MockAuthNotifier extends Mock implements AuthNotifier {}
 
 void main() {
-  // Define a test. The TestWidgets function also provides a WidgetTester
-  // to work with. The WidgetTester allows you to build and interact
-  // with widgets in the test environment.
-  testWidgets('MyWidget has a title and message', (tester) async {
-    // Test code goes here.
+  group('FavoriteSongsPage', () {
+    testWidgets('contains the PaginatedSongsListView widget', (tester) async {
+      final mockFavoriteSongRepository = MockFavoriteSongRepository();
+
+      when(() => mockFavoriteSongRepository.getFavoritePage(1)).thenAnswer(
+        (invocation) => Future.value(
+          right(
+            Fresh.yes(
+              [
+                const Song(
+                    id: 1,
+                    title: 'title',
+                    songNumber: 1,
+                    lyrics: 'lyrics',
+                    category: 'category',
+                    artist: 'artist',
+                    chords: 'chords',
+                    url: 'url'),
+              ],
+            ),
+          ),
+        ),
+      );
+
+      final mockFavoriteSongsNotifierProvider =
+          AutoDisposeStateNotifierProvider<FavoriteSongNotifier, PaginatedSongsState>(
+        (ref) => FavoriteSongNotifier(mockFavoriteSongRepository),
+      );
+
+      await tester.pumpWidget(
+        ProviderScope(
+          overrides: [favoriteSongsNotifierProvider.overrideWithProvider(mockFavoriteSongsNotifierProvider)],
+          child: const MaterialApp(
+            home: FavoriteSongsPage(),
+          ),
+        ),
+      );
+
+      final finder = find.byType(PaginatedSongsListView);
+      final paginatedSongsListView = finder.evaluate().single.widget as PaginatedSongsListView;
+      expect(
+        paginatedSongsListView.noResultsMessage,
+        "That's everything we could find in your favorite songs right now.",
+      );
+      expect(finder, findsOneWidget);
+    });
+
+    testWidgets('contains the right noResultsMessage', (tester) async {
+      final mockFavoriteSongRepository = MockFavoriteSongRepository();
+
+      when(() => mockFavoriteSongRepository.getFavoritePage(1)).thenAnswer(
+        (invocation) => Future.value(left(const BackendFailure.api(400, 'message'))),
+      );
+
+      final mockFavoriteSongsNotifierProvider =
+          AutoDisposeStateNotifierProvider<FavoriteSongNotifier, PaginatedSongsState>(
+        (ref) => FavoriteSongNotifier(mockFavoriteSongRepository),
+      );
+
+      await tester.pumpWidget(
+        ProviderScope(
+          overrides: [favoriteSongsNotifierProvider.overrideWithProvider(mockFavoriteSongsNotifierProvider)],
+          child: const MaterialApp(
+            home: FavoriteSongsPage(),
+          ),
+        ),
+      );
+
+      final finder = find.byType(PaginatedSongsListView);
+
+      final paginatedSongsListView = finder.evaluate().single.widget as PaginatedSongsListView;
+
+      expect(
+        paginatedSongsListView.noResultsMessage,
+        "That's everything we could find in your favorite songs right now.",
+      );
+    });
+    testWidgets("clicking on Sign Out button triggers provided AuthNotifier's signOut method", (tester) async {
+      final UserNotifier fakeUserNotifier = FakeUserNotifier(MockUserRepository());
+      final AuthNotifier mockAuthNotifier = MockAuthNotifier();
+      final mockFavoriteSongRepository = MockFavoriteSongRepository();
+
+      when(() => mockFavoriteSongRepository.getFavoritePage(1)).thenAnswer(
+        (invocation) => Future.value(left(const BackendFailure.api(400, 'message'))),
+      );
+
+      final mockFavoriteSongsNotifierProvider =
+          AutoDisposeStateNotifierProvider<FavoriteSongNotifier, PaginatedSongsState>(
+        (ref) => FavoriteSongNotifier(mockFavoriteSongRepository),
+      );
+
+      when(mockAuthNotifier.signOut).thenAnswer((_) => Future.value());
+
+      await tester.pumpWidget(
+        ProviderScope(
+          overrides: [
+            userNotifierProvider.overrideWithValue(
+              fakeUserNotifier,
+            ),
+            authNotifierProvider.overrideWithValue(
+              mockAuthNotifier,
+            ),
+            favoriteSongsNotifierProvider.overrideWithProvider(mockFavoriteSongsNotifierProvider)
+          ],
+          child: const MaterialApp(
+            home: FavoriteSongsPage(),
+          ),
+        ),
+      );
+
+      await tester.pump(Duration.zero);
+
+      final signOutButtonFinder = find.byKey(FavoriteSongsPageState.signOutButtonKey);
+
+      await tester.tap(signOutButtonFinder);
+
+      await tester.pump();
+
+      verify(mockAuthNotifier.signOut).called(1);
+    });
   });
 }
--- test/core/presentation/toasts_test.dart
+++ test/core/presentation/toasts_test.dart
@@ -1,11 +1,59 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+
 // Package imports:
+import 'package:flash/flash.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+// Project imports:
+import 'package:joyful_noise/core/presentation/toasts.dart';
+
 void main() {
-  // Define a test. The TestWidgets function also provides a WidgetTester
-  // to work with. The WidgetTester allows you to build and interact
-  // with widgets in the test environment.
-  testWidgets('MyWidget has a title and message', (tester) async {
-    // Test code goes here.
+  group('showNoConnectionToast', () {
+    const dialogMessage = 'Hello, there!';
+
+    testWidgets('shows the Flash widget on the screen', (tester) async {
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: Scaffold(),
+        ),
+      );
+
+      final BuildContext context = tester.element(find.byType(Scaffold));
+
+      // ignore: unawaited_futures
+      showNoConnectionToast(dialogMessage, context);
+
+      await tester.pump();
+
+      final finder = find.byType(Flash);
+
+      expect(finder, findsOneWidget);
+
+      /// Needed to let the timer for the dialog complete
+      await tester.pumpAndSettle(const Duration(seconds: 2));
+    });
+
+    testWidgets('shows the dialog message on the screen', (tester) async {
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: Scaffold(),
+        ),
+      );
+
+      final BuildContext context = tester.element(find.byType(Scaffold));
+
+      // ignore: unawaited_futures
+      showNoConnectionToast(dialogMessage, context);
+
+      await tester.pump();
+
+      final finder = find.text(dialogMessage);
+
+      expect(finder, findsOneWidget);
+
+      /// Needed to let the timer for the dialog complete
+      await tester.pumpAndSettle(const Duration(seconds: 2));
+    });
   });
 }
