23c176553f1f3919cb6a32e90f2eac0da74765f1
Jeremiah Parrack <jeremiahlukus1@gmail.com>
2022-07-26 06:53:14 -0400

    Jeremiah-Parrack | WIP search notifier

---
+++ lib/search/notifiers/search_history_notifier.dart
@@ -0,0 +1,26 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:joyful_noise/core/presentation/bootstrap.dart';
+import 'package:joyful_noise/search/infrastructure/search_history_repository.dart';
+
+class SearchHistoryNotifier extends StateNotifier<AsyncValue<List<String>>> {
+  final SearchHistoryRepository _repository;
+
+  SearchHistoryNotifier(this._repository) : super(const AsyncValue.loading());
+
+  void watchSearchTerms({String? filter}) {
+    _repository.watchSearchTerms(filter: filter).listen(
+      (data) {
+        state = AsyncValue.data(data);
+      },
+      onError: (Object error) {
+        state = AsyncValue.error(error);
+      },
+    );
+  }
+
+  Future<void> addSearchTerm(String term) => _repository.addSearchTerm(term);
+
+  Future<void> deleteSearchTerm(String term) => _repository.deleteSearchTerm(term);
+
+  Future<void> putSearchTermFirst(String term) => _repository.putSearchTermFirst(term);
+}
---
+++ test/search/notifiers/search_history_notifier_test.dart
@@ -0,0 +1,90 @@
+// Package imports:
+import 'package:dartz/dartz.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:joyful_noise/search/infrastructure/search_history_repository.dart';
+import 'package:joyful_noise/search/notifiers/search_history_notifier.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+
+// Project imports:
+import 'package:joyful_noise/backend/core/domain/backend_failure.dart';
+import 'package:joyful_noise/backend/core/domain/song.dart';
+import 'package:joyful_noise/backend/songs/core/notifiers/paginated_songs_notifier.dart';
+import 'package:joyful_noise/backend/songs/favorite_songs/infrastructure/favorite_songs_repository.dart';
+import 'package:joyful_noise/backend/songs/favorite_songs/notifiers/favorite_song_notifier.dart';
+import 'package:joyful_noise/core/domain/fresh.dart';
+
+class MockSearchHistoryRepository extends Mock implements SearchHistoryRepository {}
+
+class MockStream extends Mock implements Stream<List<String>> {}
+
+void main() {
+  group('SearchHistoryNotifier', () {
+    group('.addSearchTerm', () {
+      test('returns a AsyncLoading with no data', () async {
+        final SearchHistoryRepository mockFavoriteSongRepository = MockSearchHistoryRepository();
+        const page = 1;
+        when(() => mockFavoriteSongRepository.addSearchTerm('term')).thenAnswer(
+          (invocation) => Future.value(),
+        );
+
+        final favoriteSongNotifier = SearchHistoryNotifier(mockFavoriteSongRepository);
+
+        await favoriteSongNotifier.addSearchTerm('term');
+
+        // ignore: invalid_use_of_protected_member
+        final actualStateResult = favoriteSongNotifier.state;
+        expect(actualStateResult, const AsyncLoading<List<String>>());
+        expect(actualStateResult.asData, null);
+      });
+    });
+    group('.deleteSearchTerm', () {
+      test('returns a AsyncLoading with no data', () async {
+        final SearchHistoryRepository mockFavoriteSongRepository = MockSearchHistoryRepository();
+        const page = 1;
+        when(() => mockFavoriteSongRepository.deleteSearchTerm('term')).thenAnswer(
+          (invocation) => Future.value(),
+        );
+
+        final favoriteSongNotifier = SearchHistoryNotifier(mockFavoriteSongRepository);
+
+        await favoriteSongNotifier.deleteSearchTerm('term');
+
+        // ignore: invalid_use_of_protected_member
+        final actualStateResult = favoriteSongNotifier.state;
+        expect(actualStateResult, const AsyncLoading<List<String>>());
+        expect(actualStateResult.asData, null);
+      });
+    });
+    group('.putSearchTermFirst', () {
+      test('returns a AsyncLoading with no data', () async {
+        final SearchHistoryRepository mockFavoriteSongRepository = MockSearchHistoryRepository();
+        const page = 1;
+        when(() => mockFavoriteSongRepository.putSearchTermFirst('term')).thenAnswer(
+          (invocation) => Future.value(),
+        );
+
+        final favoriteSongNotifier = SearchHistoryNotifier(mockFavoriteSongRepository);
+
+        await favoriteSongNotifier.putSearchTermFirst('term');
+
+        // ignore: invalid_use_of_protected_member
+        final actualStateResult = favoriteSongNotifier.state;
+        expect(actualStateResult, const AsyncLoading<List<String>>());
+        expect(actualStateResult.asData, null);
+      });
+    });
+
+    group('.watchSearchTerms', () {
+      test('returns normally data when success', () async {
+        final SearchHistoryRepository mockSearchHistoryRepository = MockSearchHistoryRepository();
+
+        when(mockSearchHistoryRepository.watchSearchTerms).thenAnswer((_) => Stream.value(['query1', 'query2']));
+
+        final favoriteSongNotifier = SearchHistoryNotifier(mockFavoriteSongRepository);
+
+        expect(favoriteSongNotifier.watchSearchTerms, returnsNormally);
+      });
+    });
+  });
+}
