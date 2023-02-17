5d6d963f31728e24386026dd59fdaa4a0c5076c6
Jeremiah-Parrack <jeremiah.parrack@coxautoinc.com>
2023-02-17 10:51:45 -0500

    Jeremiah-Parrack | Tests finally work

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:joyful_noise/auth/shared/providers.dart';
@@ -8,29 +9,28 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:joyful_noise/search/presentation/search_bar.dart';
 import 'package:patrol/patrol.dart';
 
-Future<void> restoreFlutterError(Future<void> Function() call) async {
-  final originalOnError = FlutterError.onError!;
-  await call();
-  final overriddenOnError = FlutterError.onError!;
+// Future<void> restoreFlutterError(Future<void> Function() call) async {
+//   final originalOnError = FlutterError.onError!;
+//   await call();
+//   final overriddenOnError = FlutterError.onError!;
 
-  // restore FlutterError.onError
-  FlutterError.onError = (FlutterErrorDetails details) {
-    if (overriddenOnError != originalOnError) overriddenOnError(details);
-    originalOnError(details);
-  };
-}
+//   // restore FlutterError.onError
+//   FlutterError.onError = (FlutterErrorDetails details) {
+//     if (overriddenOnError != originalOnError) overriddenOnError(details);
+//     originalOnError(details);
+//   };
+// }
 
 void main() {
   patrolTest(
     'counter state is the same after going to home and switching apps',
     ($) async {
-      // await restoreFlutterError(() async {
-      //   main_dart.main();
-      // });
+      final originalOnError = FlutterError.onError;
       main_dart.main();
       for (var i = 0; i < 300; i++) {
         await $.pump();
       }
+      FlutterError.onError = originalOnError;
 
       await $(#signInButtonKey).tap();
       for (var i = 0; i < 300; i++) {
@@ -50,12 +50,10 @@ void main() {
       for (var i = 0; i < 300; i++) {
         await $.pump();
       }
+
       //final finder = find.byType(SearchBar);
       expect($(SearchBar), findsOneWidget);
-      expect($(SearchBar), findsNothing);
-      // expect(finder, findsOneWidget);
-      // final findera = find.byType(PaginatedSongsListView);
-      // expect(findera, findsNothing);
+      expect($(PaginatedSongsListView), findsOneWidget);
       await $(#signOutButtonKey).tap();
       for (var i = 0; i < 300; i++) {
         await $.pump();
