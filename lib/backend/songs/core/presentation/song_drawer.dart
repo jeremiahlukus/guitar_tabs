a848c07bbce0a0b71fd5ce9ce907c9bae2f7c91c
Jeremiah Parrack <jeremiahlukus1@gmail.com>
2023-10-06 11:29:52 -0400

    Put delete Account at bottom of drawer

--- lib/backend/songs/core/presentation/song_drawer.dart
+++ lib/backend/songs/core/presentation/song_drawer.dart
@@ -30,95 +30,113 @@ class SongDrawer extends ConsumerWidget {
       // Add a ListView to the drawer. This ensures the user can scroll
       // through the options in the drawer if there isn't enough vertical
       // space to fit everything.
-      child: ListView(
+      child: Column(
         // Important: Remove any padding from the ListView.
-        padding: EdgeInsets.zero,
+        // padding: EdgeInsets.zero,
         children: [
-          DrawerHeader(
-            child: WebsafeSvg.asset('assets/logo.svg'),
+          Expanded(
+            child: Column(
+              children: [
+                DrawerHeader(
+                  child: WebsafeSvg.asset('assets/logo.svg'),
+                ),
+                ListTile(
+                  key: athensSongBook,
+                  title: const Text('Athens Songbook'),
+                  onTap: () {
+                    AutoRouter.of(context).push(PlaylistSongsRoute(playlistName: 'Athens Songbook'));
+                    Navigator.of(context).pop();
+                  },
+                ),
+                ListTile(
+                  key: hymnal,
+                  title: const Text('Hymnal'),
+                  onTap: () {
+                    AutoRouter.of(context).push(PlaylistSongsRoute(playlistName: 'Hymnal'));
+                    Navigator.of(context).pop();
+                  },
+                ),
+                ListTile(
+                  key: blueSongbook,
+                  title: const Text('Blue Songbook'),
+                  onTap: () {
+                    AutoRouter.of(context).push(PlaylistSongsRoute(playlistName: 'Blue Songbook'));
+                    Navigator.of(context).pop();
+                  },
+                ),
+                ListTile(
+                  key: himnos,
+                  title: const Text('Himnos'),
+                  onTap: () {
+                    AutoRouter.of(context).push(PlaylistSongsRoute(playlistName: 'Himnos'));
+                    Navigator.of(context).pop();
+                  },
+                ),
+                ListTile(
+                  key: liederbuch,
+                  title: const Text('Liederbuch'),
+                  onTap: () {
+                    AutoRouter.of(context).push(PlaylistSongsRoute(playlistName: 'Liederbuch'));
+                    Navigator.of(context).pop();
+                  },
+                ),
+                ListTile(
+                  key: cantiques,
+                  title: const Text('Cantiques'),
+                  onTap: () {
+                    AutoRouter.of(context).push(PlaylistSongsRoute(playlistName: 'Cantiques'));
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            ),
           ),
-          ListTile(
-            key: athensSongBook,
-            title: const Text('Athens Songbook'),
-            onTap: () {
-              AutoRouter.of(context).push(PlaylistSongsRoute(playlistName: 'Athens Songbook'));
-              Navigator.of(context).pop();
-            },
-          ),
-          ListTile(
-            key: hymnal,
-            title: const Text('Hymnal'),
-            onTap: () {
-              AutoRouter.of(context).push(PlaylistSongsRoute(playlistName: 'Hymnal'));
-              Navigator.of(context).pop();
-            },
-          ),
-          ListTile(
-            key: blueSongbook,
-            title: const Text('Blue Songbook'),
-            onTap: () {
-              AutoRouter.of(context).push(PlaylistSongsRoute(playlistName: 'Blue Songbook'));
-              Navigator.of(context).pop();
-            },
-          ),
-          ListTile(
-            key: himnos,
-            title: const Text('Himnos'),
-            onTap: () {
-              AutoRouter.of(context).push(PlaylistSongsRoute(playlistName: 'Himnos'));
-              Navigator.of(context).pop();
-            },
-          ),
-          ListTile(
-            key: liederbuch,
-            title: const Text('Liederbuch'),
-            onTap: () {
-              AutoRouter.of(context).push(PlaylistSongsRoute(playlistName: 'Liederbuch'));
-              Navigator.of(context).pop();
-            },
-          ),
-          ListTile(
-            key: cantiques,
-            title: const Text('Cantiques'),
-            onTap: () {
-              AutoRouter.of(context).push(PlaylistSongsRoute(playlistName: 'Cantiques'));
-              Navigator.of(context).pop();
-            },
-          ),
-          ListTile(
-            key: deleteUser,
-            title: const Text('Delete Account'),
-            onTap: () {
-              // ignore: inference_failure_on_function_invocation
-              showDialog(
-                context: context,
-                builder: (_) {
-                  return AlertDialog(
-                    title: const Center(child: Text('Delete Account')),
-                    content: const Text(
-                      '⚡️DANGER!!⚡️\n\nAre you sure you want to delete your account?\nThis cannot be undone.\n\n⚡️DANGER!!⚡️',
-                      textAlign: TextAlign.center,
-                    ),
-                    actions: <Widget>[
-                      TextButton(
-                        key: yesDeleteUser,
-                        onPressed: () {
-                          ref.read(userRemoteServiceProvider).deleteUser();
-                          ref.read(authNotifierProvider.notifier).signOut();
-                          Navigator.of(context).pop();
-                        },
-                        child: const Text('YES'),
-                      ),
-                      TextButton(
-                        key: noDeleteUser,
-                        onPressed: () => Navigator.pop(context),
-                        child: const Text('NO'),
-                      ),
-                    ],
-                  );
-                },
-              );
-            },
+          // Put delete Account at bottom of drawer
+          Align(
+            alignment: FractionalOffset.bottomCenter,
+            child: Column(
+              children: [
+                const Divider(),
+                ListTile(
+                  key: deleteUser,
+                  title: const Text(
+                    'Delete Account',
+                    style: TextStyle(color: Colors.red),
+                  ),
+                  onTap: () {
+                    // ignore: inference_failure_on_function_invocation
+                    showDialog(
+                      context: context,
+                      builder: (_) {
+                        return AlertDialog(
+                          title: const Center(child: Text('Delete Account')),
+                          content: const Text(
+                            '⚡️DANGER!!⚡️\n\nAre you sure you want to delete your account?\nThis cannot be undone.\n\n⚡️DANGER!!⚡️',
+                            textAlign: TextAlign.center,
+                          ),
+                          actions: <Widget>[
+                            TextButton(
+                              key: yesDeleteUser,
+                              onPressed: () {
+                                ref.read(userRemoteServiceProvider).deleteUser();
+                                ref.read(authNotifierProvider.notifier).signOut();
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text('YES'),
+                            ),
+                            TextButton(
+                              key: noDeleteUser,
+                              onPressed: () => Navigator.pop(context),
+                              child: const Text('NO'),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                ),
+              ],
+            ),
           ),
         ],
       ),
