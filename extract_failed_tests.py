933846b32cac4a94bbc0cc70ee36c226e9ffe710
Jeremiah Parrack <jeremiahlukus1@gmail.com>
2023-11-12 14:28:23 -0500

    modify extract ffailed tests pyton

--- extract_failed_tests.py
+++ extract_failed_tests.py
@@ -5,8 +5,9 @@ if os.path.exists('test_results.txt'):
     with open('test_results.txt', 'r') as file:
         data = file.read()
 
-    failed_tests = re.findall(r'FAILED: .+', data)
+    failed_tests = re.findall(r'The following TestFailure was thrown running a test:(.*?Which:.*?)\n', data, re.DOTALL)
 
     with open('failed_tests.txt', 'w') as file:
-        for test in failed_tests:
-            file.write(test + '\n')
+        for i, test in enumerate(failed_tests, start=1):
+            file.write(f'# {i} FAILURE\n{test.strip()}\n\n')
+        file.write(f'Total failures: {len(failed_tests)}\n')
